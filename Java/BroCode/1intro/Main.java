3f68c4946609e6d3387eccd5cb006b717307f29c
Apoorv Chandrakar <apoorv.learn@gmail.com>
2025-12-29 15:41:34 -0500

    Setting up the environment

---
+++ Java/BroCode/1intro/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args){
+        // This is a comment!!
+        System.out.print("I like pizza!");
+        System.out.println("It's really good!!");
+        System.out.print("This is the end with end char\n");
+        System.out.println("Final!!");
+    }
+}
